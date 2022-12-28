1ef69a6d83bb1ad614b19996aa92be8824b774ca
Emmanuel-Q <78229107+Emmanuel-Q@users.noreply.github.com>
2022-12-28 10:12:53 +0000

    added classes, elements and metadata

---
+++ Data/SchoolManagementDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolManagementApp.MVC.Data;
+
+public partial class SchoolManagementDbContext : DbContext
+{
+    public SchoolManagementDbContext(DbContextOptions<SchoolManagementDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC07B31BD301");
+
+            entity.HasIndex(e => e.IndexNumber, "UQ__Students__98DAB2EA111336A1").IsUnique();
+
+            entity.Property(e => e.DateOfBirth).HasColumnType("date");
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
